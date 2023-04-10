63f613a2bf3d2d2e1da0214dfcc239d44336a6a1
10 <tentacloids@gmail.com>
2023-04-10 21:03:25 +0100

    cargo update + more numerics

--- src/builtins/numeric.rs
+++ src/builtins/numeric.rs
@@ -8,47 +8,6 @@ use super::util;
 use crate::base::*;
 use crate::interpreter::*;
 
-// TODO pop two at once with stack_pop::<(T, T)>
-
-/// Add the top two numbers on the stack.
-pub fn add<T: std::ops::Add<T, Output=T> + Value + Clone>(i: &mut Interpreter) -> BuiltinRet {
-    let b = i.stack_pop::<T>()?;
-    let a = i.stack_pop::<T>()?;
-    i.stack_push(a.into_inner() + b.into_inner());
-    Ok(())
-}
-
-/// `a b sub` is `a - b`.
-/// If you need to remember which way around it is, `sub` is `negate add`.
-/// All of these operations are the same order as infix, so
-/// `a b op` infix is `a op b`.
-pub fn sub<T: std::ops::Sub<T, Output=T> + Value + Clone>(i: &mut Interpreter) -> BuiltinRet {
-    let b = i.stack_pop::<T>()?;
-    let a = i.stack_pop::<T>()?;
-    i.stack_push(a.into_inner() - b.into_inner());
-    Ok(())
-}
-
-/// `a b mul` is `a * b`.
-pub fn mul<T: std::ops::Mul<T, Output=T> + Value + Clone>(i: &mut Interpreter) -> BuiltinRet {
-    let b = i.stack_pop::<T>()?;
-    let a = i.stack_pop::<T>()?;
-    i.stack_push(a.into_inner() * b.into_inner());
-    Ok(())
-}
-
-/// `a b div` is `a / b`.
-/// Integer division rounds towards zero.
-/// If you need to remember which way around it is, `div` is `recip mul` for f64s.
-/// All of these operations are the same order as infix, so
-/// `a b op` infix is `a op b`.
-pub fn div<T: std::ops::Div<T, Output=T> + Value + Clone>(i: &mut Interpreter) -> BuiltinRet {
-    let b = i.stack_pop::<T>()?;
-    let a = i.stack_pop::<T>()?;
-    i.stack_push(a.into_inner() / b.into_inner());
-    Ok(())
-}
-
 /// Division that produces a `false` `error?`
 /// instead of crashing when dividing by 0.
 /// See [div] for more information.
@@ -60,27 +19,6 @@ pub fn div_nozero<T: std::ops::Div<T, Output=T> + Value + Clone + std::cmp::Part
     } else {
         i.stack_push(a / b);
     }
-    Ok(())
-}
-
-/// Negate the number on top of the stack.
-pub fn negate<T: std::ops::Neg<Output=T> + Value + Clone>(i: &mut Interpreter) -> BuiltinRet {
-    let a = i.stack_pop::<T>()?;
-    i.stack_push(-a.into_inner());
-    Ok(())
-}
-
-/// Calculate the absolute value of the i64 on top of the stack.
-pub fn abs_i64(i: &mut Interpreter) -> BuiltinRet {
-    let a = i.stack_pop::<i64>()?;
-    i.stack_push(a.into_inner().abs());
-    Ok(())
-}
-
-/// Calculate the absolute value of the f64 on top of the stack.
-pub fn abs_f64(i: &mut Interpreter) -> BuiltinRet {
-    let a = i.stack_pop::<f64>()?;
-    i.stack_push(a.into_inner().abs());
     Ok(())
 }
 
@@ -98,6 +36,24 @@ pub fn f64_to_i64(i: &mut Interpreter) -> BuiltinRet {
     Ok(())
 }
 
+fn unop<T: Value + Clone, R: Value>(f: impl Fn(T) -> R) -> impl Fn(&mut Interpreter) -> BuiltinRet {
+    move |i| {
+        let a = i.stack_pop::<T>()?;
+        i.stack_push(f(a.into_inner()));
+        Ok(())
+    }
+}
+
+/// All binary operations are the same order as infix, so `a b op` infix is `a op b`.
+fn binop<T: Value + Clone, R: Value>(f: impl Fn(T, T) -> R) -> impl Fn(&mut Interpreter) -> BuiltinRet {
+    move |i| {
+        let b = i.stack_pop::<T>()?;
+        let a = i.stack_pop::<T>()?;
+        i.stack_push(f(a.into_inner(), b.into_inner()));
+        Ok(())
+    }
+}
+
 /// Install numeric functions for i64 and f64
 pub fn install(i: &mut Interpreter) {
     util::add_type_predicate_builtin::<i64>(i, "i64?");
@@ -105,6 +61,9 @@ pub fn install(i: &mut Interpreter) {
 
     i.add_builtin("i64->string", util::value_tostring_debug::<i64>);
     i.add_builtin("f64->string", util::value_tostring_debug::<f64>);
+
+    i.add_builtin("i64->f64", i64_to_f64);
+    i.add_builtin("f64->i64", f64_to_i64);
 
     i.add_builtin("i64-equal", util::equality::<i64>);
     i.add_builtin("f64-equal", util::equality::<f64>);
@@ -114,27 +73,39 @@ pub fn install(i: &mut Interpreter) {
     // f64 not hashable
     // i.add_builtin("f64-hash", util::value_hash::<f64>);
 
-    i.add_builtin("i64-add", add::<i64>);
-    i.add_builtin("f64-add", add::<f64>);
-    i.add_builtin("i64-sub", sub::<i64>);
-    i.add_builtin("f64-sub", sub::<f64>);
-    i.add_builtin("i64-mul", mul::<i64>);
-    i.add_builtin("f64-mul", mul::<f64>);
+    i.add_builtin("i64-add", binop::<i64, i64>(|a, b| a + b));
+    i.add_builtin("f64-add", binop::<f64, f64>(|a, b| a + b));
+    i.add_builtin("i64-sub", binop::<i64, i64>(|a, b| a - b));
+    i.add_builtin("f64-sub", binop::<f64, f64>(|a, b| a - b));
+    i.add_builtin("i64-mul", binop::<i64, i64>(|a, b| a * b));
+    i.add_builtin("f64-mul", binop::<f64, f64>(|a, b| a * b));
     i.add_builtin("i64-div", div_nozero::<i64>);
-    i.add_builtin("f64-div", div::<f64>);
+    i.add_builtin("f64-div", binop::<f64, f64>(|a, b| a / b));
 
-    i.add_builtin("i64-negate", negate::<i64>);
-    i.add_builtin("f64-negate", negate::<f64>);
+    i.add_builtin("i64-negate", unop::<i64, i64>(|a| -a));
+    i.add_builtin("f64-negate", unop::<f64, f64>(|a| -a));
 
-    // quotient
-    // remainder
-    // quotient/  remainder
-    // modulo
+    i.add_builtin("i64-remainder", binop::<i64, i64>(|a, b| a % b));
 
-    i.add_builtin("i64-abs", abs_i64);
-    i.add_builtin("f64-abs", abs_f64);
+    i.add_builtin("i64-abs", unop::<i64, i64>(|a| a.abs()));
+    i.add_builtin("f64-abs", unop::<f64, f64>(|a| a.abs()));
 
-    i.add_builtin("i64->f64", i64_to_f64);
-    i.add_builtin("f64->i64", f64_to_i64);
+    i.add_builtin("f64-sqrt", unop::<f64, f64>(|a| a.sqrt()));
+
+    i.add_builtin("f64-sin", unop::<f64, f64>(|a| a.sin()));
+    i.add_builtin("f64-cos", unop::<f64, f64>(|a| a.cos()));
+    i.add_builtin("f64-tan", unop::<f64, f64>(|a| a.tan()));
+    i.add_builtin("f64-asin", unop::<f64, f64>(|a| a.asin()));
+    i.add_builtin("f64-acos", unop::<f64, f64>(|a| a.acos()));
+    i.add_builtin("f64-atan", unop::<f64, f64>(|a| a.atan()));
+
+    i.add_builtin("i64-bitand", binop::<i64, i64>(|a, b| a & b));
+    i.add_builtin("i64-bitxor", binop::<i64, i64>(|a, b| a ^ b));
+    i.add_builtin("i64-bitor", binop::<i64, i64>(|a, b| a | b));
+    i.add_builtin("i64-bitnot", unop::<i64, i64>(|a| !a));
+
+    i.add_builtin("i64-ashift", binop::<i64, i64>(|a, b| {
+        if b > 0 { a << b } else { a >> -b }
+    }));
 }
 
