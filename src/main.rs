3dfc8b63e143b377c091e8383a02dfe27b276fb5
10 <tentacloids@gmail.com>
2023-08-16 20:42:09 +0100

    Start zipexe init.w or open argv[1]

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,75 @@
 
+use std::io::{ self, Read };
+use std::fs::File;
 use std::process::ExitCode;
+
+use palaver;
+use zip::{ read::ZipArchive, result::ZipError };
+
+use worst::reader;
 use worst::interpreter::*;
 use worst::builtins;
-use worst::base::*;
 
-fn basic_printerr(i: &mut Interpreter, v: &Val) {
-    if let Some(v) = v.downcast_ref::<Symbol>() {
-        eprint!("{v}");
-    } else if let Some(v) = v.downcast_ref::<bool>() {
-        eprint!("{}", if *v { "#t" } else { "#f" });
-    } else if let Some(v) = v.downcast_ref::<String>() {
-        eprint!("{v:?}");
-    } else if let Some(v) = v.downcast_ref::<i64>() {
-        eprint!("{v}");
-    } else if let Some(v) = v.downcast_ref::<f64>() {
-        eprint!("{v}");
-    } else if let Some(v) = v.downcast_ref::<List>() {
-        eprint!("(");
-        for v in v.iter() {
-            basic_printerr(i, v);
-            eprint!(" ");
-        }
-        eprint!(")");
-    } else if v.is::<TypeId>() {
-        let name = i.uniques_mut().get_type::<String>();
-        if let Some(v) = v.meta_ref().get_val(&name) {
-            eprint!("{}", v.downcast_ref::<String>().unwrap());
-        } else {
-            eprint!("<type>");
-        }
+fn error(msg: &str) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, msg)
+}
+
+fn zip_open_self() -> io::Result<Option<ZipArchive<File>>> {
+    let exe = palaver::env::exe()?;
+    match ZipArchive::new(exe) {
+        Ok(z) => Ok(Some(z)),
+        Err(ZipError::Io(e)) => Err(e),
+        Err(ZipError::FileNotFound) => Err(io::Error::new(io::ErrorKind::NotFound, "not found")),
+        Err(ZipError::InvalidArchive(_) | ZipError::UnsupportedArchive(_)) => Ok(None),
+    }
+}
+
+fn open_zip_initw() -> io::Result<Option<Interpreter>> {
+    let Some(mut archive) = zip_open_self()? else { return Ok(None); };
+    let mut initw = match archive.by_name("init.w") {
+        Ok(f) => f,
+        Err(ZipError::Io(e)) => return Err(e),
+        Err(ZipError::FileNotFound) => return Ok(None),
+        Err(ZipError::InvalidArchive(msg)) => 
+            return Err(io::Error::new(io::ErrorKind::InvalidData, msg)),
+        Err(ZipError::UnsupportedArchive(msg)) => 
+            return Err(io::Error::new(io::ErrorKind::Other, msg)),
+    };
+    let mut s = String::new();
+    initw.read_to_string(&mut s)?;
+    let body = reader::read_all(&mut s.chars()).unwrap();
+    Ok(Some(Interpreter::new(body)))
+}
+
+fn open_arg1() -> io::Result<Option<Interpreter>> {
+    let Some(f) = std::env::args().nth(1) else { return Ok(None); };
+    let contents = std::fs::read_to_string(f)?;
+    let body = reader::read_all(&mut contents.chars()).unwrap();
+    Ok(Some(Interpreter::new(body)))
+}
+
+fn find_entrypoint() -> io::Result<Interpreter> {
+    if let Some(r) = open_zip_initw()? { return Ok(r); }
+    if let Some(r) = open_arg1()? { return Ok(r); }
+    Err(error("no entrypoint"))
+}
+
+fn main_ioresult() -> io::Result<()> {
+    let mut i = find_entrypoint()?;
+    builtins::install(&mut i);
+    if let Err(_e) = i.run() {
+        Err(error("interpreter error"))
     } else {
-        eprint!("(value)");
+        Ok(())
     }
 }
 
 fn main() -> ExitCode {
-    let mut i = worst::embedded();
-    builtins::install(&mut i);
-    if let Err(e) = i.run() {
-        // if IsError::is_error(&e) {
-        //     eprint!("\nTop-level error: ");
-        // }
-        basic_printerr(&mut i, &e);
-        eprint!("\nStack: ");
-        for v in i.stack_ref().clone().iter() {
-            basic_printerr(&mut i, v);
-            eprint!(" ");
-        }
-        eprintln!("\nCall stack:");
-        let u = i.uniques_mut().get_type::<Symbol>();
-        let stack = i.stack_meta_refs().map(|m| m.get_val(&u));
-        for name in stack {
-            if let Some(name) = name {
-                eprintln!("  {}", name.downcast_ref::<Symbol>().unwrap());
-            } else {
-                eprintln!("  ???");
-            }
-        }
-        return ExitCode::FAILURE;
+    if let Err(e) = main_ioresult() {
+        eprintln!("{}", e);
+        ExitCode::FAILURE
+    } else {
+        ExitCode::SUCCESS
     }
-
-    ExitCode::SUCCESS
 }
 
